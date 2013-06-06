dd1439583323fd9f117ccaa6694e4ad2d822d631
Dane Hale <danehale0612@gmail.com>
2013-06-06 09:33:45 -0500

    delete public/index.html added validation to form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,12 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create( params[:task] )
-    flash[:notice] = "Your task has been added!"
-    redirect_to :root
+    if @task.save
+      flash[:notice] = "Your task has been added!"
+      redirect_to :root
+    else
+      flash[:notice] = "Name field Empty. Could not add Task."
+      redirect_to :root
+    end
   end
 end
